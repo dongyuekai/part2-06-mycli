89279dfb05e84e8a532b96c7a328d0350996d20c
“dongyuekai” <457545648@qq.com>
2021-06-10 19:03:34 +0800

    inquirer递进问题

---
+++ inquirer/test.js
@@ -0,0 +1,44 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+const questList = [
+  {
+    type: 'input', // 指明当前问题式输入性
+    name: 'userName', // 这里的name 将来会用于从结果数据中解构我们当前的输入值
+    message: '请输入您的姓名',
+    // default: chalk.blue('dyk'),
+    validate(val) {
+      if (!val) {
+        return '当前内容为必填项'
+      } else {
+        return true
+      }
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'isInstall',
+    message: '是否安装依赖'
+  },
+  {
+    type: 'list', // 单选
+    name: 'method',
+    message: '请选择您的下载工具',
+    choices: ['npm', 'cnpm', 'yarn'],
+    when(a) {
+      if (a.isInstall) {
+        return true
+      }
+      return false
+    }
+  }
+]
+
+// 所有问题都处理完的结果
+inquirer.prompt(questList).then(answer => {
+  console.log(answer)
+})
+
+/**
+ * 01
+*/
